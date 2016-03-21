ebcc07bbf13390d2ee93832b11de4efddf29e4c7
Kevin Simard <kev.simard@gmail.com>
2016-03-21 18:28:23 -0400

    Fix exception handler to always return json response

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -3,7 +3,12 @@
 namespace App\Exceptions;
 
 use Exception;
+use Illuminate\Auth\Access\AuthorizationException;
+use Illuminate\Database\Eloquent\ModelNotFoundException;
 use Illuminate\Foundation\Exceptions\Handler as CoreHandler;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpKernel\Exception\HttpException;
+use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
 
 class Handler extends CoreHandler
 {
@@ -29,10 +34,37 @@ class Handler extends CoreHandler
     /**
      * @param  \Illuminate\Http\Request  $request
      * @param  \Exception  $e
-     * @return \Illuminate\Http\Response
+     * @return \Illuminate\Http\JsonResponse
      */
     public function render($request, Exception $e)
     {
-        return parent::render($request, $e);
+        $code = $e->getCode();
+        $message = $e->getMessage();
+
+        if ($e instanceof ModelNotFoundException) {
+            $e = new NotFoundHttpException($e->getMessage(), $e);
+        } elseif ($e instanceof AuthorizationException) {
+            $e = new HttpException(403, $e->getMessage());
+        } elseif (method_exists($e, 'getResponse') && $e->getResponse()) {
+            $code = $e->getResponse()->getStatusCode();
+            $message = $e->getResponse()->getContent();
+        }
+
+        if ($this->isHttpException($e)) {
+            $code = $e->getStatusCode();
+            $message = $this->getMessageByCode($code);
+        }
+
+        return response()->json(compact('message'), $code);
+    }
+
+    /**
+     * @param  int  $code
+     * @return string
+     */
+    protected function getMessageByCode($code)
+    {
+        return isset(Response::$statusTexts[$code])
+            ? Response::$statusTexts[$code] : 'Unknown Status';
     }
 }
