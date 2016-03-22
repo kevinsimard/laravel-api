a3901f715011159d6aac7165444c22558e2420bc
Kevin Simard <kev.simard@gmail.com>
2016-03-21 21:47:07 -0400

    Change api token length to 80 characters

--- app/Modules/Auth/Http/Controllers/AuthController.php
+++ app/Modules/Auth/Http/Controllers/AuthController.php
@@ -33,7 +33,7 @@ class AuthController extends BaseController
             'name' => $data['name'],
             'email' => $data['email'],
             'password' => $data['password'],
-            'api_token' => str_random(60),
+            'api_token' => str_random(80),
         ]);
     }
 }
