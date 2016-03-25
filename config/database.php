434da126ce4c360800b86689a5467d63ea6e009c
Kevin Simard <kev.simard@gmail.com>
2016-03-25 10:01:43 -0400

    Allow for setting sqlite database via env

--- config/database.php
+++ config/database.php
@@ -11,7 +11,7 @@ return [
     'connections' => [
         'sqlite' => [
             'driver' => 'sqlite',
-            'database' => ':memory:',
+            'database' => env('DB_DATABASE', database_path('database.sqlite')),
             'prefix' => '',
         ],
 
