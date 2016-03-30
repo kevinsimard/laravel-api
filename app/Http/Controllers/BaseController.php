355facddc1422d6b2d9118190f703113becd00b0
Kevin Simard <kev.simard@gmail.com>
2016-03-29 20:38:08 -0400

    Add validate method in BaseController

--- app/Http/Controllers/BaseController.php
+++ app/Http/Controllers/BaseController.php
@@ -4,10 +4,26 @@ namespace App\Http\Controllers;
 
 use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
 use Illuminate\Foundation\Bus\DispatchesJobs;
-use Illuminate\Foundation\Validation\ValidatesRequests;
 use Illuminate\Routing\Controller;
+use Illuminate\Validation\ValidationException;
 
 abstract class BaseController extends Controller
 {
-    use AuthorizesRequests, DispatchesJobs, ValidatesRequests;
+    use AuthorizesRequests, DispatchesJobs;
+
+    /**
+     * @param  array  $data
+     * @param  array  $rules
+     * @return void
+     *
+     * @throws \Illuminate\Validation\ValidationException
+     */
+    protected function validate(array $data, array $rules)
+    {
+        $validator = \Validator::make($data, $rules);
+
+        if ($validator->fails()) {
+            throw new ValidationException($validator);
+        }
+    }
 }
