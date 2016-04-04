ca73e55b141a6ce891bc51aa945deb134434431e
Kevin Simard <kev.simard@gmail.com>
2016-04-04 16:39:37 -0400

    Add support for the AuthorizesRequests trait

--- app/Http/Controllers/BaseController.php
+++ app/Http/Controllers/BaseController.php
@@ -3,10 +3,11 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
+use Illuminate\Foundation\Auth\Access\AuthorizesResources;
 use Illuminate\Foundation\Bus\DispatchesJobs;
 use Illuminate\Routing\Controller;
 
 abstract class BaseController extends Controller
 {
-    use AuthorizesRequests, DispatchesJobs;
+    use AuthorizesRequests, AuthorizesResources, DispatchesJobs;
 }
