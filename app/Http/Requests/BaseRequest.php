58d4463f81dfe336d2a5f93b910d9d62d5899944
Kevin Simard <kev.simard@gmail.com>
2016-04-04 08:06:24 -0400

    Override failedValidation method

--- app/Http/Requests/BaseRequest.php
+++ app/Http/Requests/BaseRequest.php
@@ -2,7 +2,9 @@
 
 namespace App\Http\Requests;
 
+use Illuminate\Contracts\Validation\Validator;
 use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Validation\ValidationException;
 
 abstract class BaseRequest extends FormRequest
 {
@@ -20,5 +22,16 @@ abstract class BaseRequest extends FormRequest
     protected function passesAuthorization()
     {
         return true;
+    }
+
+    /**
+     * @param  \Illuminate\Contracts\Validation\Validator  $validator
+     * @return void
+     *
+     * @throws \Illuminate\Validation\ValidationException
+     */
+    protected function failedValidation(Validator $validator)
+    {
+        throw new ValidationException($validator);
     }
 }
