eb13e6d6c7e203e0dbac28eb4476c3be7c83891c
Kevin Simard <kev.simard@gmail.com>
2016-04-11 17:14:11 -0400

    Add middleware to enable CORS requests

---
+++ app/Http/Middleware/CorsRequest.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Http\Middleware;
+
+class CorsRequest
+{
+    /**
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \Closure  $next
+     * @return mixed
+     */
+    public function handle($request, \Closure $next)
+    {
+        $response = $next($request);
+
+        foreach ([
+            'Access-Control-Allow-Origin' => '*',
+            'Access-Control-Allow-Methods' => 'GET, POST, PUT, PATCH, DELETE, OPTIONS',
+            'Access-Control-Allow-Headers' => 'Content-Type',
+        ] as $key => $value) {
+            $response->header($key, $value);
+        }
+
+        return $response;
+    }
+}
