8fd1234c2944db7446c3350f1716b2e05371a37f
Kevin Simard <kev.simard@gmail.com>
2016-04-20 09:14:58 -0400

    Add ValidatesRequests trait to BaseController

--- app/Http/Controllers/BaseController.php
+++ app/Http/Controllers/BaseController.php
@@ -5,9 +5,10 @@ namespace App\Http\Controllers;
 use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
 use Illuminate\Foundation\Auth\Access\AuthorizesResources;
 use Illuminate\Foundation\Bus\DispatchesJobs;
+use Illuminate\Foundation\Validation\ValidatesRequests;
 use Illuminate\Routing\Controller;
 
 abstract class BaseController extends Controller
 {
-    use AuthorizesRequests, AuthorizesResources, DispatchesJobs;
+    use AuthorizesRequests, AuthorizesResources, DispatchesJobs, ValidatesRequests;
 }
